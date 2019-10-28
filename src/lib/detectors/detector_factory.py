c502cb7146557e0ea3832f15329959625f97ad65
naodev <naoki.morita@gmail.com>
2019-10-28 11:15:42 +0900

    add ARCDetector

--- src/lib/detectors/detector_factory.py
+++ src/lib/detectors/detector_factory.py
@@ -5,11 +5,13 @@ from __future__ import print_function
 from .exdet import ExdetDetector
 from .ddd import DddDetector
 from .ctdet import CtdetDetector
+from .arc import ARCDetector
 from .multi_pose import MultiPoseDetector
 
 detector_factory = {
   'exdet': ExdetDetector, 
   'ddd': DddDetector,
   'ctdet': CtdetDetector,
-  'multi_pose': MultiPoseDetector, 
+  'arc': ARCDetector,
+  'multi_pose': MultiPoseDetector,
 }

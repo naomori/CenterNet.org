7a92612d54e0cb86330b4275b97fcf105b44b147
naodev <naoki.morita@gmail.com>
2019-10-31 17:12:25 +0900

    fix test scripts

--- src/test_arc_dla_34.sh
+++ src/test_arc_dla_34.sh
@@ -1,4 +1,4 @@
-#python test.py arc --dataset arc --exp_id arc_dla_34 --arch dla_34 --keep_res  --num_workers 4 --load_model ../exp/arc/arc_dla_34/model_last.pth
-python test.py arc --dataset arc --exp_id arc_dla_34 --keep_res --resume
-python test.py arc --dataset arc --exp_id arc_dla_34 --keep_res --resume --flip_test
-python test.py arc --dataset arc --exp_id arc_dla_34 --keep_res --resume --flip_test --test_scales 0.5,0.75,1,1.25,1.5
+#python test.py arc --dataset arc --exp_id arc_dla_34 --arch dla_34 --keep_res --num_workers 4 --load_model ../exp/arc/arc_dla_34/model_last.pth
+python test.py arc --dataset arc --exp_id arc_dla_34 --keep_res --resume --num_workers 4 --load_model ../exp/arc/arc_dla_34/model_last.pth
+python test.py arc --dataset arc --exp_id arc_dla_34 --keep_res --resume --num_workers 4 --flip_test --load_model ../exp/arc/arc_dla_34/model_last.pth
+python test.py arc --dataset arc --exp_id arc_dla_34 --keep_res --resume --num_workers 4 --flip_test --test_scales 0.5,0.75,1,1.25,1.5 --load_model ../exp/arc/arc_dla_34/model_last.pth
